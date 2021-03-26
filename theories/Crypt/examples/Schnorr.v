f77fac547e98c812e5a1c54a9d906283dfae9de5
Nikolaj Sidorenco <Nikolaj176@hotmail.com>
2021-03-26 13:30:32 +0100

    Extract general notion of Σ-Protocols

--- theories/Crypt/examples/Schnorr.v
+++ theories/Crypt/examples/Schnorr.v
@@ -12,7 +12,7 @@ From Mon Require Import SPropBase.
 From Crypt Require Import Axioms ChoiceAsOrd SubDistr Couplings
   UniformDistrLemmas FreeProbProg Theta_dens RulesStateProb UniformStateProb
   pkg_core_definition chUniverse pkg_composition pkg_rhl Package Prelude
-  pkg_notation AsymScheme.
+  pkg_notation SigmaProtocol.
 
 From Coq Require Import Utf8.
 From extructures Require Import ord fset fmap.
@@ -28,6 +28,8 @@ Set Primitive Projections.
 
 Local Open Scope ring_scope.
 Import GroupScope GRing.Theory.
+
+Import PackageNotation.
 
 Parameter gT : finGroupType.
 Definition ζ : {set gT} := [set : gT].
@@ -81,25 +83,7 @@ Proof.
   reflexivity.
 Qed.
 
-Inductive probEmpty : Type → Type := .
-
-Module Type SigmaProtocolParams.
-
-  Parameter Witness Statement Message Challenge Response State : finType.
-  Parameter w0 : Witness.
-  Parameter e0 : Challenge.
-  Parameter z0 : Response.
-  Parameter R : Statement -> Witness -> bool.
-
-End SigmaProtocolParams.
-
-Module Type SigmaProtocolRules (π : SigmaProtocolParams).
-
-  Import π.
-
-End SigmaProtocolRules.
-
-Module SchnorrParams <: SigmaProtocolParams.
+Module MyParam <: SigmaProtocolParams.
 
   Definition Witness : finType := [finType of 'Z_q].
   Definition Statement : finType := FinGroup.arg_finType gT.
@@ -116,135 +100,65 @@ Module SchnorrParams <: SigmaProtocolParams.
   Definition R : Statement -> Witness -> bool :=
     (fun (h : Statement) (w : Witness) => h == (g ^+ w)).
 
-End SchnorrParams.
+End MyParam.
+
+Module MyAlg <: SigmaProtocolAlgorithms MyParam.
+
+  Import MyParam.
+
+  Instance positive_gT : Positive #|gT|.
+  Proof.
+    apply /card_gt0P. exists g. auto.
+  Qed.
+
+  Instance Witness_pos : Positive #|Witness|.
+  Proof.
+    apply /card_gt0P. exists w0. auto.
+  Qed.
+
+  Definition Statement_pos : Positive #|Statement| := _.
+  Definition Message_pos : Positive #|Message| := _.
+  Definition Challenge_pos : Positive #|Challenge| := _.
+  Definition Response_pos : Positive #|Response| := _.
+  Definition State_pos : Positive #|State| := _.
+  
+  Definition choiceWitness : chUniverse := 'fin #|Witness|.
+  Definition choiceStatement : chUniverse := 'fin #|Statement|.
+  Definition choiceMessage : chUniverse := 'fin #|Message|.
+  Definition choiceChallenge : chUniverse := 'fin #|Challenge|.
+  Definition choiceResponse : chUniverse := 'fin #|Response|.
+  Definition choiceTranscript : chUniverse := chProd (chProd choiceMessage choiceChallenge) choiceResponse.
+  Definition choiceState := 'fin #|State|.
+
+  Definition i_witness := #|Witness|.
+
+  Definition Commit {L : {fset Location}} (h : choiceStatement) (w : choiceWitness):
+    code L [interface] (choiceMessage × choiceState) :=
+    {code
+       r ← sample uniform i_witness ;;
+       ret (fto (g ^+ otf r), r)
+    }.
+
+  Definition Response {L : {fset Location}} (h : choiceStatement) (w : choiceWitness) (r : choiceState) (a : choiceMessage) (e : choiceChallenge):
+    code L [interface] choiceResponse :=
+    {code
+       ret (fto (otf r + otf e * otf w))
+    }.
+
+  Definition Simulate {L : {fset Location}} (h : choiceStatement) (e : choiceChallenge):
+    code L [interface] choiceTranscript :=
+    {code
+       z ← sample uniform i_witness ;;
+       ret (fto (g ^+ (otf z) * (otf h ^- (otf e))), e, z)
+    }.
+
+End MyAlg.
 
 Local Open Scope package_scope.
 
-Import SchnorrParams.
+Module Schnorr := SigmaProtocol MyParam MyAlg.
 
-Include (SigmaProtocolRules SchnorrParams).
-
-Import PackageNotation.
-
-Instance positive_gT : Positive #|gT|.
-Proof.
-  apply /card_gt0P. exists g. auto.
-Qed.
-
-Instance positive_Witness : Positive #|Witness|.
-Proof.
-  apply /card_gt0P. exists w0. auto.
-Qed.
-
-Definition choiceWitness : chUniverse := 'fin #|Witness|.
-Definition choiceStatement : chUniverse := 'fin #|Statement|.
-Definition choiceMessage : chUniverse := 'fin #|Message|.
-Definition choiceChallenge : chUniverse := 'fin #|Challenge|.
-Definition choiceResponse : chUniverse := 'fin #|Response|.
-Definition choiceTranscript : chUniverse := chProd (chProd choiceMessage choiceChallenge) choiceResponse.
-Definition choiceState := choiceWitness.
-
-Definition i_witness := #|Witness|.
-Definition i_challenge := #|Challenge|.
-  
-Definition INIT : nat := 3.
-Definition CHALLENGE : nat := 4.
-Definition RESPONSE : nat := 5.
-Definition VERIFY : nat := 6.
-Definition SIM : nat := 7.
-Definition RUN : nat := 8.
-
-Notation " 'chStatement' " :=
-  choiceStatement
-  (in custom pack_type at level 2).
-Notation " 'chMessage' " :=
-  choiceMessage
-  (in custom pack_type at level 2).
-Notation " 'chCommit' " :=
-  (chProd choiceMessage choiceState)
-  (in custom pack_type at level 2).
-Notation " 'chChallenge' " :=
-  choiceChallenge
-  (in custom pack_type at level 2).
-Notation " 'chResponse' " :=
-  choiceResponse
-  (in custom pack_type at level 2).
-Notation " 'chFst' " :=
-  (chProd (chProd choiceMessage choiceState) choiceChallenge)
-  (in custom pack_type at level 2).
-Notation " 'chTranscript' " :=
-  choiceTranscript
-  (in custom pack_type at level 2).
-
-Definition SigmaProtocol (h : choiceStatement) (w : choiceWitness):
-  package fset0
-  [interface]
-  [interface val #[ INIT ] : 'unit → chCommit ;
-             (* val #[ CHALLENGE ] : chMessage → chChallenge ; *)
-             val #[ RESPONSE ] : chFst → chResponse
-             (* val #[ VERIFY ] : chTranscript → 'bool *)
-  ] :=
-  [package
-    def #[ INIT ] (_ : 'unit) : chCommit {
-     r ← sample uniform i_witness ;;
-     ret (fto (g ^+ (otf r)), r)
-    } ;
-
-    (* def #[ CHALLENGE ] (a : chMessage) : chChallenge { *)
-    (*  e ← sample uniform i_challenge ;; *)
-    (*  ret e *)
-    (* } ; *)
-
-    def #[ RESPONSE ] (are : chFst) : chResponse {
-     let '(tmp,e) := are in
-     let '(a,r) := tmp in
-     ret (fto (otf r + otf e * otf w))
-    } 
-  ].
-
-Definition Simulator (h : choiceStatement):
-  package fset0
-   [interface]
-   [interface val #[ SIM ] : chChallenge → chTranscript] :=
-  [package
-   def #[ SIM ] (e : chChallenge) : chTranscript {
-     z ← sample uniform i_witness ;;
-     ret (fto (g ^+ (otf z) * (otf h ^- (otf e))), e, z)
-  }].
-
-Definition RUN_real (e : choiceChallenge):
-  package fset0
-    [interface val #[ INIT ] : 'unit → chCommit ;
-               val #[ RESPONSE ] : chFst → chResponse ]
-    [interface val #[ RUN ] : 'unit → chTranscript] :=
-  [package
-   def #[ RUN ] (_: 'unit) : chTranscript
-    {
-      #import {sig #[ INIT ] : 'unit → chCommit } as msg ;;
-      #import {sig #[ RESPONSE ] : chFst → chResponse } as rspns ;;
-      m ← msg Datatypes.tt ;;
-      let '(a, r) := m in
-      z ← rspns (m, e) ;;
-      ret (a,e,z)
-    }
-  ].
-
-Definition RUN_ideal (e : choiceChallenge):
-  package fset0
-    [interface val #[ SIM ] : chChallenge → chTranscript ]
-    [interface val #[ RUN ] : 'unit → chTranscript] :=
-  [package
-   def #[ RUN ] (_: 'unit) : chTranscript
-    {
-      #import {sig #[ SIM ] : chChallenge → chTranscript } as sim ;;
-      t ← sim e ;;
-      ret t
-    }
-  ].
-
-Definition SHVZK h w e :
-  loc_GamePair [interface val #[ RUN ] : 'unit → chTranscript] :=
-  fun b => if b then {locpackage (RUN_real e ∘ (SigmaProtocol h w))} else {locpackage (RUN_ideal e ∘ (Simulator h))}.
+Import MyParam MyAlg Schnorr.
 
 #[local] Definition f (e w : Witness) :
   Arit (uniform i_witness) → Arit (uniform i_witness) :=
@@ -257,39 +171,48 @@ Proof.
   - apply prime_gt1, prime_order. 
 Qed.
 
-Lemma real_ideal_equiv h w e:
-  R (otf h) (otf w) = true ->
-  (SHVZK h w e) true ≈₀ (SHVZK h w e) false.
+Lemma bij_f w e : bijective (f w e).
+Proof.
+  pose f' := f e w.
+  subst f'. unfold f.
+  exists (fun x => (fto (otf x - w * e))).
+  - intro x; unfold fto, otf; rewrite !enum_rankK GRing.addrK enum_valK; reflexivity. 
+  - intro x; unfold fto, otf; rewrite !enum_rankK GRing.subrK enum_valK; reflexivity.
+Qed.
+
+Lemma Schnorr_SHVZK h w e:
+  R (otf h) (otf w) →
+  ∀ LA A, 
+    ValidPackage LA [interface val #[ RUN ] : 'unit → chTranscript] A_export A →
+    fdisjoint LA (SHVZK h w e true).(locs) →
+    fdisjoint LA (SHVZK h w e false).(locs) →
+    Advantage (SHVZK h w e) A = 0.
 Proof.
   unfold R=> rel. apply reflection_nonsense in rel.
-  eapply eq_rel_perf_ind_eq.
+  intros LA A Hvalid Hdis1 Hdis2.
+  rewrite Advantage_E.
+  apply: eq_rel_perf_ind_eq.
+  2,3: assumption.
   simplify_eq_rel m.
   ssprove_code_link_commute. simpl.
   simplify_linking.
   (* Programming logic part *)
-  pose f' := f (otf e) (otf w).
-  assert (bij_f : bijective f').
-  { subst f'. unfold f.
-    exists (fun x => (fto (otf x - (otf e) * (otf w)))).
-    - intro x; unfold fto, otf; rewrite !enum_rankK GRing.addrK enum_valK; reflexivity. 
-    - intro x; unfold fto, otf; rewrite !enum_rankK GRing.subrK enum_valK; reflexivity.
-  }
-  eapply r_uniform_bij with (1 := bij_f)=> z_val.
+  eapply r_uniform_bij with (1 := bij_f (otf w) (otf e))=> z_val.
   apply r_ret.
   (* Ambient logic proof of post condition *)
   intros s0 s1 Hs.
-  unfold f', f.
-  rewrite -!expgnE rel.
+  unfold f.
+  rewrite - !expgnE rel.
   split.
   2: apply Hs. 
-  simpl. f_equal.
+  simpl.
   rewrite otf_fto expg_mod.
   2: rewrite order_ge1; apply expg_order.
-  rewrite expgD -!expgVn.
+  rewrite expgD - !expgVn.
   rewrite group_prodC group_prodA group_prodC group_prodA /=.
   rewrite expg_mod.
   2 : rewrite order_ge1; apply expg_order.
-  rewrite expgM expgAC -!expgM -expgMn.
-  - rewrite mulgV expg1n mul1g. reflexivity.
+  rewrite -expgM -expgMn.
+  - rewrite mulgV expg1n mul1g Zp_mulC. reflexivity.
   - apply group_prodC.
 Qed.
---
+++ theories/Crypt/examples/SigmaProtocol.v
@@ -0,0 +1,136 @@
+
+From Relational Require Import OrderEnrichedCategory GenericRulesSimple.
+
+Set Warnings "-notation-overridden,-ambiguous-paths".
+From mathcomp Require Import all_ssreflect all_algebra reals distr realsum
+  ssrnat ssreflect ssrfun ssrbool ssrnum eqtype choice seq.
+Set Warnings "notation-overridden,ambiguous-paths".
+
+From Crypt Require Import Axioms ChoiceAsOrd SubDistr Couplings
+  UniformDistrLemmas FreeProbProg Theta_dens RulesStateProb UniformStateProb
+  pkg_core_definition chUniverse pkg_composition pkg_rhl
+  Package Prelude.
+
+From Coq Require Import Utf8.
+From extructures Require Import ord fset fmap.
+
+Set Bullet Behavior "Strict Subproofs".
+Set Default Goal Selector "!".
+Set Primitive Projections.
+
+Import Num.Def.
+Import Num.Theory.
+Import mc_1_10.Num.Theory.
+
+Import PackageNotation.
+
+Local Open Scope ring_scope.
+
+Module Type SigmaProtocolParams.
+
+  Parameter Witness Statement Message Challenge Response State : finType.
+  Parameter w0 : Witness.
+  Parameter e0 : Challenge.
+  Parameter z0 : Response.
+  Parameter R : Statement -> Witness -> bool.
+
+End SigmaProtocolParams.
+
+Module Type SigmaProtocolAlgorithms (π : SigmaProtocolParams).
+
+  Import π.
+
+  Local Open Scope package_scope.
+
+  Parameter Statement_pos : Positive #|Statement|.
+  Parameter Witness_pos : Positive #|Witness|.
+  Parameter Message_pos : Positive #|Message|.
+  Parameter Challenge_pos : Positive #|Challenge|.
+  Parameter Response_pos : Positive #|Response|.
+  Parameter State_pos : Positive #|State|.
+
+  #[local] Existing Instance Statement_pos.
+  #[local] Existing Instance Witness_pos.
+  #[local] Existing Instance Message_pos.
+  #[local] Existing Instance Challenge_pos.
+  #[local] Existing Instance Response_pos.
+  #[local] Existing Instance State_pos.
+
+  Definition choiceWitness : chUniverse := 'fin #|Witness|.
+  Definition choiceStatement : chUniverse := 'fin #|Statement|.
+  Definition choiceMessage : chUniverse := 'fin #|Message|.
+  Definition choiceChallenge : chUniverse := 'fin #|Challenge|.
+  Definition choiceResponse : chUniverse := 'fin #|Response|.
+  Definition choiceTranscript : chUniverse := chProd (chProd choiceMessage choiceChallenge) choiceResponse.
+  Definition choiceState := 'fin #|State|.
+
+  Parameter Commit :
+    ∀ {L : {fset Location}} (h : choiceStatement) (w : choiceWitness),
+      code L [interface] (choiceMessage × choiceState).
+
+  Parameter Response :
+    ∀ {L : {fset Location}} (h : choiceStatement) (w : choiceWitness) (s : choiceState) (a : choiceMessage) (e : choiceChallenge),
+      code L [interface] choiceResponse.
+
+  Parameter Simulate :
+    ∀ {L : {fset Location}} (h : choiceStatement) (e : choiceChallenge),
+      code L [interface] choiceTranscript.
+
+  (*TODO: Add Challenge, Verify, and Extractor procedures. *)
+
+  Notation " 'chStatement' " := choiceStatement (in custom pack_type at level 2).
+  Notation " 'chMessage' " := choiceMessage (in custom pack_type at level 2).
+  Notation " 'chCommit' " := (chProd choiceMessage choiceState) (in custom pack_type at level 2).
+  Notation " 'chChallenge' " := choiceChallenge (in custom pack_type at level 2).
+  Notation " 'chResponse' " := choiceResponse (in custom pack_type at level 2).
+  Notation " 'chFst' " :=
+    (chProd (chProd choiceMessage choiceState) choiceChallenge)
+      (in custom pack_type at level 2).
+
+End SigmaProtocolAlgorithms.
+
+Module SigmaProtocol (π : SigmaProtocolParams)
+                     (Alg : SigmaProtocolAlgorithms π).
+
+  Import π.
+  Import Alg.
+
+  (* Compatibitlity *)
+  Notation " 'chTranscript' " := choiceTranscript (in custom pack_type at level 2).
+  Definition i_witness := #|Witness|.
+  Definition RUN : nat := 0.
+
+
+  Local Open Scope package_scope.
+
+  Definition RUN_real (h : choiceStatement) (w : choiceWitness) (e : choiceChallenge):
+    package fset0
+      [interface]
+      [interface val #[ RUN ] : 'unit → chTranscript] :=
+    [package
+     def #[ RUN ] (_: 'unit) : chTranscript
+      {
+        m ← Commit h w ;;
+        let '(a, s) := m in
+        z ← Response h w s a e ;;
+        ret (a,e,z)
+      }
+    ].
+  
+  Definition RUN_ideal (h : choiceStatement) (e : choiceChallenge):
+    package fset0
+      [interface]
+      [interface val #[ RUN ] : 'unit → chTranscript] :=
+    [package
+     def #[ RUN ] (_: 'unit) : chTranscript
+      {
+        t ← Simulate h e ;;
+        ret t
+      }
+    ].
+  
+  Definition SHVZK h w e :
+    loc_GamePair [interface val #[ RUN ] : 'unit → chTranscript] :=
+    fun b => if b then {locpackage (RUN_ideal h e)} else {locpackage (RUN_real h w e)}.
+
+End SigmaProtocol.
